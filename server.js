7eae3a6bfa539272078c6d7b9c141bd8d309aa80
eliav <eliavmaman2@gmail.com>
2018-07-17 00:17:26 +0300

    asd

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+// const express = require('express');
+// const app = express();
+//
+// app.use(express.static(__dirname + '/dist'));
+//
+// app.listen(3000, () => console.log('Example app listening on port 3000!'))
+//
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+//
+// });
+//
+// console.log('Server is running');
 const express = require('express');
 const app = express();
 
+// Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-
-});
-
-console.log('Server is running');
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
